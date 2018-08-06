63ce9543f52ff303b27b382cf356b1fa804c9dcc
Unknown <notoriousgorilla@gmail.com>
2018-08-06 18:58:50 +0200

    view fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
     
     root 'pillars#index'
     
-    get 'signup'  => 'users#new' 
+    get '/signup'  => 'users#new' 
     resources :users
     
     get '/pillar' => 'pillars#show'
     
     get '/login' => 'sessions#new'
-    post 'login' => 'sessions#create'
-    delete 'logout' => 'sessions#destroy'
+    post '/login' => 'sessions#create'
+    delete '/logout' => 'sessions#destroy'
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
