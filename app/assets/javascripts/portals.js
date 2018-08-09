478311b628f1c9b37d9e3fc78fbcbd3fbdf76e73
Unknown <notoriousgorilla@gmail.com>
2018-08-09 17:34:27 +0200

    Style landing site

--- app/assets/javascripts/portals.js
+++ app/assets/javascripts/portals.js
@@ -1,10 +1,14 @@
 $(document).ready(() => {
     
-    var $alphaButton = $('.alphas-list');
+    var $alphaButton = $('.header-inner-alphas');
+    var $aboutButton = $('.header-inner-about');
+    var $locateButton = $('.header-inner-locate');
     
     var $alphaList = $('.content-main-alphas');
     var $placeHolder1 = $('.content-main-placeholder1');
     var $placeHolder2 = $('.content-main-placeholder2');
+    
+    $aboutButton.addClass('active-tab-main');
     
     $placeHolder1.show();
     $placeHolder2.hide();
@@ -14,6 +18,18 @@ $(document).ready(() => {
         $alphaList.show();
         $placeHolder1.hide();
         $placeHolder2.hide();
+    });
+    
+    $aboutButton.on('click', () => {
+        $alphaList.hide();
+        $placeHolder1.show();
+        $placeHolder2.hide();
+    });
+    
+    $locateButton.on('click', () => {
+        $alphaList.hide();
+        $placeHolder1.hide();
+        $placeHolder2.show();
     });
     
         
