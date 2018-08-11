2cb796c9afc621e0e818b65f3a7266cb623d3136
Unknown <notoriousgorilla@gmail.com>
2018-08-11 10:57:59 +0200

    db test
    edited seed.rb for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 mateo = User.create(first_name: 'Mateo', last_name: 'Lazo', email: 'mateo@email.com', password: 'Mateo1', password_confirmation: 'Mateo1', role: 'editor')
 julian = User.create(first_name: 'Julian', last_name: 'Jones', email: 'julian@email.com', password: 'Julian1', password_confirmation: 'Julian1')
 freida = User.create(first_name: 'Freida', last_name: 'Gray', email: 'freida@email.com', password: 'Freida1', password_confirmation: 'Freida1', role: 'admin')
+
+bilvask = Alpha.create(name: 'Bilvask')
