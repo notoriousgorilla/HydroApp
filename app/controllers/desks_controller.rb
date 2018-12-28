47e379c2508a0ee9537f2970ffc2bbd032a30592
Lars Hemmingby <notoriousgorilla@gmail.com>
2018-12-28 14:14:24 +0100

    AlphaA

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -1,2 +1,9 @@
 class DesksController < ApplicationController
+    
+    def show
+        @alphas = Alpha.all
+        @desks = Desk.limit(2) #limit to 2 results
+        @desk = Desk.limit(2).order('id desc') #limit to 2 results with descending order
+    end
+    
 end
